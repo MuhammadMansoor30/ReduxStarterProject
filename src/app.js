1fb332fe27f3fd5339319eb5ea197cd1d8cc0cff
MuhammadMansoor30 <mm1704682@gmail.com>
2024-09-15 12:38:53 +0500

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function app() {
+    console.log('Redux Starter Project');
+}
+
+export default app;
+
+// NOTES:
+// For Compiltaion of webpack we will use add webpack to package.json file as start or build instead of index.js file.
+// For Starting of project we will use webpack serve --open as start command in package.json.
